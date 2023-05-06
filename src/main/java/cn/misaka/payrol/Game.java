ef2fcb25aa276fb728c5ec8c70f571ca8103667c
tangbin <tangbin@hzzhebei.com>
2023-05-06 15:09:16 +0800

    增加显示每轮得分的测试和实现

--- src/main/java/cn/misaka/payrol/Game.java
+++ src/main/java/cn/misaka/payrol/Game.java
@@ -7,13 +7,26 @@ package cn.misaka.payrol;
  */
 public class Game {
 
+    private int itsScore = 0;
+    private int[] itsThrows = new int[21];
+    private int itsCurrentThrow = 0;
+
+
     public int score() {
         return itsScore;
     }
 
     public void add(int pins) {
+        itsThrows[itsCurrentThrow++] = pins;
         itsScore += pins;
     }
 
-    private int itsScore = 0;
+    public int scoreForFrame(int frame) {
+        int score = 0;
+        for (int ball = 0; frame > 0 && (ball < itsCurrentThrow); ball += 2, frame--) {
+            score += itsThrows[ball] + itsThrows[ball + 1];
+        }
+        return score;
+    }
+
 }
--- src/main/java/cn/misaka/payrol/TestFrame.java
+++ src/main/java/cn/misaka/payrol/TestFrame.java
@@ -37,4 +37,15 @@ public class TestFrame extends TestCase {
         assertEquals(9, g.score());
     }
 
+    public void testFourThrowsNoMark() {
+        Game g = new Game();
+        g.add(5);
+        g.add(4);
+        g.add(7);
+        g.add(2);
+        assertEquals(18, g.score());
+        assertEquals(9, g.scoreForFrame(1));
+        assertEquals(18, g.scoreForFrame(2));
+    }
+
 }
