754af688dd425793dd06450a7ef15e657041b2ad
tangbin <tangbin@hzzhebei.com>
2023-05-06 15:16:50 +0800

    优化初始化Game到setUp函数中

--- src/main/java/cn/misaka/payrol/TestFrame.java
+++ src/main/java/cn/misaka/payrol/TestFrame.java
@@ -24,28 +24,4 @@ public class TestFrame extends TestCase {
         assertEquals(5, f.getScore());
     }
 
-    public void testOneThrows() {
-        Game g = new Game();
-        g.add(5);
-        assertEquals(5, g.score());
-    }
-
-    public void testTwoThrowsNoMark() {
-        Game g = new Game();
-        g.add(5);
-        g.add(4);
-        assertEquals(9, g.score());
-    }
-
-    public void testFourThrowsNoMark() {
-        Game g = new Game();
-        g.add(5);
-        g.add(4);
-        g.add(7);
-        g.add(2);
-        assertEquals(18, g.score());
-        assertEquals(9, g.scoreForFrame(1));
-        assertEquals(18, g.scoreForFrame(2));
-    }
-
 }
---
+++ src/main/java/cn/misaka/payrol/TestGame.java
@@ -0,0 +1,43 @@
+package cn.misaka.payrol;
+
+import junit.framework.TestCase;
+
+/**
+ * TestGame
+ *
+ * @author tangbin
+ */
+public class TestGame extends TestCase {
+
+    public TestGame(String name) {
+        super(name);
+    }
+
+    private Game g;
+
+    @Override
+    public void setUp() {
+        g = new Game();
+    }
+
+    public void testOneThrows() {
+        g.add(5);
+        assertEquals(5, g.score());
+    }
+
+    public void testTwoThrowsNoMark() {
+        g.add(5);
+        g.add(4);
+        assertEquals(9, g.score());
+    }
+
+    public void testFourThrowsNoMark() {
+        g.add(5);
+        g.add(4);
+        g.add(7);
+        g.add(2);
+        assertEquals(18, g.score());
+        assertEquals(9, g.scoreForFrame(1));
+        assertEquals(18, g.scoreForFrame(2));
+    }
+}
